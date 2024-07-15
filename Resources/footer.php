07c2d9035d16ca1d117e3efc5b5a64923f305322
Mapi <mapitenar@hotmail.com>
2024-07-15 17:05:43 +0200

    Index + header + footer

---
+++ Resources/footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foro D&D</title>
+    <link rel="shortcut icon" href="favicon.png">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<footer>
+    <div class="footer">
+        <h4>Dungeons & Dragons Foro</h4>
+        <h5>Siguenos en nuestras redes sociales</h5>
+        <div class="socialMedia">
+            <a href=""><img src="Resources/facebooklogo.png" alt=""></a>
+            <a href=""><img src="Resources/twitterlogo.png" alt=""></a>
+            <a href=""><img src="Resources/instagramlogo.png" alt=""></a>
+        </div>
+        <ul class="footerList">
+            <li class="linksFooter">
+                <a href="#">Contacto</a></li>
+            <li>
+                <a href="#">Aviso legal</a></li>
+            <li>
+                <a href="#">Privacidad</a></li>
+            <li>
+                <a href="#">Terminos de servicio</a></li>
+
+        </ul>
+        <p class="copyright">© 2024  Todos los derechos reservados</p>
+
+    </div>
+</footer>
+</body>
+
+</html>
